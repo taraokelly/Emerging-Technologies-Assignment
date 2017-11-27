46543237300fb9fe78a4bb5d9db18c7897d800f9
taraokelly <taraokelly.dev@gmail.com>
2017-11-27 23:10:07 +0000

    Upload post request, project restructure

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 import os
+import flask
 from flask import Flask, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -9,7 +10,7 @@ UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 # Start and configure application.
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Routing: @app.route is a decorator used to match URLs to view functions in Flask apps.
@@ -47,22 +48,7 @@ def upload_file():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('uploaded_file',
-            #                       filename=filename))
-            return redirect(request.url)
+        image = request.values["image"]
     return redirect(request.url)
 
 def allowed_file(filename):
